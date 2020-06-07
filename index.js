8fc2231d8773adb5b3cce298aa1c1c3310446efc
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-06-07 20:18:55 +0200

    hero section done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
 const burger = document.querySelector('.burger');
 const navList = document.querySelector('.nav-list');
 burger.addEventListener('click', () => {
   navList.classList.toggle('open');
   burger.classList.toggle('open');
 })
+function tellTime() {
+  const element = document.querySelector('.time-text');
+  const date = new Date().toString();
+  const hour = date.substring(16,21);
+  console.log(hour);
+  element.textContent = hour + ` we're close`;
+}
+tellTime();
